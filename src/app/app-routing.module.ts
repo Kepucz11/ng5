91657d25c2c6d81d095cf93038c8fa481f9f6462
Adam Rylski <adam.rylski@atos.net>
2018-04-03 12:54:24 +0200

    Added login with Google

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
-
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SignupComponent } from './signup/signup.component';
+import { EmailComponent } from './email/email.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: HomeComponent
-  },
-  {
-    path: 'about/:id',
-    component: AboutComponent
-  },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about/:id', component: AboutComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'email-login', component: EmailComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'profile', component: ProfileComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,20 +10,42 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { DataService } from './data.service';
 import { enableProdMode } from '@angular/core';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { EmailComponent } from './email/email.component';
+import { ProfileComponent } from './profile/profile.component';
+import { AuthService } from './auth.service';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDadcOeh2JHaAn8fb7UxIKDMXCcdNAgnzk",
+  authDomain: "ng5-tutorial-f8bc2.firebaseapp.com",
+  databaseURL: "https://ng5-tutorial-f8bc2.firebaseio.com",
+  projectId: "ng5-tutorial-f8bc2",
+  storageBucket: "ng5-tutorial-f8bc2.appspot.com",
+  messagingSenderId: "951972847486"
+};
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    LoginComponent,
+    SignupComponent,
+    EmailComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule
   ],
-  providers: [DataService],
+  providers: [DataService, AuthService],
   bootstrap: [AppComponent]
 
 })
